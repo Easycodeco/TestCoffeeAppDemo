5872bf58d085f90a0454cc4a2cd8582f04051a24
Mubarak <co.easycode@gmail.com>
2025-02-24 13:29:08 +0300

    Initial Commit

---
+++ TestCoffeeAppDemo/TestCoffeeAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCoffeeAppDemoApp.swift
+//  TestCoffeeAppDemo
+//
+//  Created by M3 on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestCoffeeAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
